f681aac66f2b745a1090388b13db4ed87eb22d99
Ashley Brooks <ashley.brooks.a@gmail.com>
2017-09-28 16:28:42 -0700

    Declaring preprocessor statements so this can run on mac, linux, or windows

--- repl.c
+++ repl.c
@@ -1,31 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <editline/readline.h>
-// #include <editline/history.h>
+// If we are compiling on Windows, compile these functions
+#ifdef _WIN32
+#include <string.h>
 
-// Declare a buffer for user input of size 2048
-static char input[2048];
+static char buffer[2048];
+
+// Fake readline function:
+char* readline(char* prompt) {
+	fputs(prompt, stdout);
+	fgets(buffer, 2048, stdin);
+	char* cpy = malloc(strlen(buffer)+1);
+	strcpy(cpy, buffer);
+	cpy[strlen(cpy)-1] = '\0';
+	return cpy;
+}
+
+// Fake add_history function:
+void add_history(char* unused) {}
+
+// Otherwise, include the editline headers:
+#else
+#include <editline/readline.h>
+#endif
 
 int main(int argc, char** argv) {
 
-	// Print Version and Exit information
 	puts("Lispy Version 0.0.0.0.1");
 	puts("Press ctrl+c to exit\n");
 
-	// In a never ending loop:
 	while (1) {
 
-		// Output our prompt and get input
+		// Now, in either case readline will be correctly defined
 		char* input = readline("lispy> ");
-
-		// Add input to history to record it
 		add_history(input);
 
-		// Echo input back to user
 		printf("No, you're a %s\n", input);
-
-		// Free retrieved input
 		free(input);
 	}
 
