d8a77f163c47bdb146a816e36fa31bc75dd8f981
Ashley Brooks <ashley.brooks.a@gmail.com>
2017-10-01 16:19:45 -0700

    Adding a grammar for polish notation

--- parsing.c
+++ parsing.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "mpc.h"
 
 // If we are compiling on Windows, compile these functions
 #ifdef _WIN32
@@ -27,6 +28,22 @@ void add_history(char* unused) {}
 
 int main(int argc, char** argv) {
 
+	// Create some parsers
+	mpc_parser_t* Number = mpc_new("number");
+	mpc_parser_t* Operator = mpc_new("operator");
+	mpc_parser_t* Expr = mpc_new("expr");
+	mpc_parser_t* Lispy = mpc_new("lispy");
+
+	// Now defining the parsers with the following language:
+	mpca_lang(MPCA_LANG_DEFAULT,
+		" \
+			number : /-?[0-9]+/ ; \
+			operator : '+' | '-' | '*' | '/' ; \
+			expr : <number> | '(' <operator> <expr>+ ')' ; \
+			lispy : /^/ <operator> <expr>+ /$/ ; \
+		",
+		Number, Operator, Expr, Lispy);
+
 	puts("Lispy Version 0.0.0.0.1");
 	puts("Press ctrl+c to exit\n");
 
@@ -36,10 +53,23 @@ int main(int argc, char** argv) {
 		char* input = readline("lispy> ");
 		add_history(input);
 
-		printf("No, you're a %s\n", input);
+		mpc_result_t r;
+		if (mpc_parse("<stdin>", input, Lispy, &r)) {
+			// On success print the AST
+			mpc_ast_print(r.output);
+			mpc_ast_delete(r.output);
+		} else {
+			// Otherwise print the error
+			mpc_err_print(r.error);
+			mpc_err_delete(r.error);
+		}
+
 		free(input);
 	}
 
+	// Undefine and delete the parsers
+	mpc_cleanup(4, Number, Operator, Expr, Lispy);
+	
 	return 0;
 
 }
